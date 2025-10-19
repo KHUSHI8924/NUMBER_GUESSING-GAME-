30ed3e3703be92043060ab4218137060591b052e
KHUSHI8924 <khushidriven@gmail.com>
2025-10-19 19:08:58 +0530

    add comment

--- guess.py
+++ guess.py
@@ -2,15 +2,20 @@ import random
 
 print("NUMBER GUESSING GAME \n Let's play and unwind the game.")
 
+# randint function to generate the random number between 1 to 9 
 number=random.randint(1,9)
 
+#number of chances given to player 
 chances=0
 
 print("Guess the number between 1 and 9:")
 
 while True:
+    
+    # Enter a number between 1 to 9
     guess=int(input())
     
+    #Compare the user enter number with the number to be guessed
     if guess==number:
         print(f"Congratulations!! You guess the number {number} in {chances} attempts!")
         break
